58f95266852a8ec4bab523bae9b7ad1b26a67462
Marcos Antônio de Souza Silva <chuckymass@gmail.com>
2016-02-11 08:11:18 -0300

    Using material design with new template

---
+++ javascripts/init.js
@@ -0,0 +1,7 @@
+(function($){
+  $(function(){
+    $('.button-collapse').sideNav();
+    $('.parallax').parallax();
+    // $("#nav-mobile").append($('#menu').clone().contents());
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
