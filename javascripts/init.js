f4dcb17edbb6846f068a70331f6afb2991df87f6
Marcos A. de S. Silva <chuckymass@gmail.com>
2016-07-20 13:47:29 -0300

    Adiciona efeito particula

--- javascripts/init.js
+++ javascripts/init.js
@@ -1,7 +1,15 @@
-(function($){
-  $(function(){
-    $('.button-collapse').sideNav();
-    $('.parallax').parallax();
-    // $("#nav-mobile").append($('#menu').clone().contents());
-  }); // end of document ready
+(function ($) {
+    'use strict';
+    $(document).ready(function () {
+        $('.button-collapse').sideNav();
+        $('.parallax').parallax();
+
+        particleground($('#index-banner')[0], {
+          dotColor: '#5cbdaa',
+          lineColor: '#5cbdaa'
+        });
+
+        var intro = $('#profile')[0];
+        intro.style.marginTop = - intro.offsetHeight + 'px';
+    }); // end of document ready
 })(jQuery); // end of jQuery name space
